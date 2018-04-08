23cc03636733f652a2a2dcac131da5827807fc39
Levent Yilmaz <yilmaz@auburn.edu>
2018-04-07 23:47:18 -0500

    Created the inference package

--- src/main/scala/featuremodel/FeatureAlgebra.scala
+++ src/main/scala/featuremodel/FeatureAlgebra.scala
@@ -12,7 +12,6 @@ trait FeatureAlg[E] {
   var featureName : String
   val featureSet : Set[String]
   def Def(x:List[Activity]) : E
-  def Add(e1:E, e2: E) : E
   def Cons() : E
   def Cons(a : Agent) : E
 }
@@ -31,7 +30,7 @@ class MkLifter[A,B](f : (A,B) => A with B) extends Lifter[A,B] {
 }
 
 
-trait FeatureMerge[A,B] extends FeatureAlg[A with B] {
+trait FeatureMerge[A,B] extends FeatureAlg[A] {
   val lifter : Lifter[A,B]
   val alg1 : FeatureAlg[A]
   val alg2 : FeatureAlg[B]
@@ -43,10 +42,6 @@ trait FeatureMerge[A,B] extends FeatureAlg[A with B] {
 
   def Def(x: List[Activity]): A with B = {
     lifter.lift(alg1.Def(x), alg2.Def(x))
-  }
-
-  def Add(e1: A with B, e2: A with B) : A with B = {
-    lifter.lift(alg1.Add(e1, e2), alg2.Add(e1, e2))
   }
 
   def Cons() : A with B = {
@@ -90,19 +85,7 @@ trait BaseModelGen extends FeatureAlg[Feature] {
     }
   }
 
-  def Add(e1: Feature, e2: Feature): Feature = new BaseModel {
-    var featureName = "base"
-    var agent : Agent = null
-    var behavior: List[Activity] = e1.Val() ::: e2.Val()
 
-    def Val(): List[Activity] = {
-      behavior
-    }
-
-    def eVal(): Unit = {
-      for (x <- behavior) x.execute()
-    }
-  }
 
   def Cons(): Feature = new BaseModel {
     var agent : Agent = null
@@ -143,7 +126,7 @@ object base extends BaseModelGen
 
 object baseGenerator extends FeatureMerge[Feature,Feature] {
   var featureName = "base"
-  val featureSet = Set()
+  val featureSet : Set[String] = Set()
   val alg1 = base
   val alg2 = base
   val lifter = LiftBase
