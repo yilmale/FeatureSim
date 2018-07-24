649757e6603099660083789ceb3e87387983ce51
Levent Yilmaz <yilmaz@auburn.edu>
2018-07-24 09:01:10 -0500

    feature collaboration template

--- src/main/scala/metasim/MetaTest.scala
+++ src/main/scala/metasim/MetaTest.scala
@@ -28,10 +28,17 @@ object MetaTest {
   var classList : Set[Defn.Class] = Set()
   val pattern = "([A-Za-z0-9]*)_([A-Za-z0-9]+)".r
 
-  def initializeBase(classes: List[Defn.Class], featureName: Term.Name): Unit = {
+  def initialize(objs: List[Defn.Object]): Unit = {
+    objs foreach {o => {
+        var cls = o collect {case cl: Defn.Class => cl}
+        initializeFeature(cls,o.name)
+      }
+    }
+  }
+
+  def initializeFeature(classes: List[Defn.Class], featureName: Term.Name): Unit = {
     classes foreach { cl =>
     {
-
       if (!(classDefined.keys exists(c=>c.name.toString() == cl.name.toString()))) {
         classDefined += (cl -> true)
         var cName = Type.Name(featureName+"_"+cl.name.toString)
@@ -49,6 +56,18 @@ object MetaTest {
                 }
                 """
       }
+      else {
+        var cName = Type.Name(featureName+"_"+cl.name.toString)
+        var basecName = Type.Name("Base_" + cl.name.toString())
+        var baseType = Init(basecName,Name(""),Nil)
+        var cStats = cl.templ.stats
+        classList = classList  +
+          q"""class $cName extends $baseType {
+                 var Super = null
+                 ..$cStats
+                }
+                """
+      }
     }
     }
   }
@@ -57,12 +76,10 @@ object MetaTest {
 
   }
 
-  def featureMerge1(base: Defn.Object, lifter: Defn.Object): Defn.Object = {
+  def featureMerge(base: Defn.Object, lifter: Defn.Object): Defn.Object = {
     var fts0 = base.collect {case cls: Defn.Class => cls}
     var fts1 = lifter.collect {case cls: Defn.Class => cls}
 
-    initializeBase(fts0,base.name)
-    initializeBase(fts1,lifter.name)
 
     val pattern(prefix,className) = "Feature1Feature2_Graph"
 
@@ -89,104 +106,13 @@ object MetaTest {
 
   }
 
-  def featureMerge(baseFeature: Defn.Object, liftFeature: Defn.Object): Defn.Object = {
-    var fts0 = baseFeature.collect {case cls: Defn.Class => cls}
-    var fts1 = liftFeature.collect {case cls: Defn.Class => cls}
 
-    var joint = (for (cl <- fts0) yield cl.name.toString()) intersect
-      (for (cl <- fts1) yield cl.name.toString())
-    println("Joint classes are " + joint)
-
-    var baseMinusLift = (for (cl <- fts0) yield cl.name.toString()) diff
-      (for (cl <- fts1) yield cl.name.toString())
-    println("Base minus Lift is " + baseMinusLift)
-    var liftMinusBase = (for (cl <- fts1) yield cl.name.toString()) diff
-      (for (cl <- fts0) yield cl.name.toString())
-    println("Lift minus base is " + liftMinusBase)
-
-    var classList = List[Defn.Class]()
-    fts0 foreach {c =>
-    {
-      var cName = c.name
-      if (joint exists (x => x == cName.toString()))
-      {
-        var rStats = List[Stat]()
-        fts1 foreach { r => {
-          var rName = r.name
-          if (cName.toString() == rName.toString())
-            {
-              rStats=r.templ.stats
-            }
-          }
-        }
-
-        var cstats = c.templ.stats ::: rStats
-        classList =
-          q"""class $cName
-           {
-             ..$cstats
-           }""" :: classList
-
-      }
-      else {
-        var cstats = c.templ.stats
-        classList =
-            q"""class $cName
-           {
-             ..$cstats
-           }""" :: classList
-      }
-    }
-    }
-
-    var classList1 = List[Defn.Class]()
-    baseMinusLift foreach { bml =>
-      {
-        fts0 foreach { cl =>
-          {
-            var cName = cl.name
-            if (cl.name.toString() == bml) {
-              var cstats = cl.templ.stats
-              classList1 =
-                q"""class $cName
-                {
-                  ..$cstats
-                }""" :: classList1
-            }
-          }
-        }
-      }
-    }
-
-    liftMinusBase foreach { bml =>
-    {
-      fts1 foreach { cl =>
-      {
-        var cName = cl.name
-        if (cl.name.toString() == bml) {
-          var cstats = cl.templ.stats
-          classList1 =
-            q"""class $cName
-                {
-                  ..$cstats
-                }""" :: classList1
-            }
-         }
-        }
-      }
-    }
-
-    classList = classList ::: classList1
-
-    var composite : Defn.Object = q"object CM {..$classList}"
-    composite
-  }
 
   def apply(): Unit = {
 
     val f =  source"""  import Collaboration._
-  object featurea {
-   abstract class Graph {
+  object base {
+   class Graph {
     var a1 : Int = 0
     var a2 : Int = 1
 
@@ -199,13 +125,13 @@ object MetaTest {
        var y = 10
     }
    }
-   abstract class Node {
+   class Node {
      def test2() : Int = {
       var y = 10
      }
    }
 
-   abstract class Edge {
+   class Edge {
       var e : Int = 15
    }
 
@@ -239,16 +165,17 @@ object MetaTest {
 }"""
 
 
-    var clss = f.collect { case cls: Defn.Object  => cls }
-    var cf = featureMerge1(clss(0),clss(1))
-    println(cf)
+    var clss = f.collect { case cls: Defn.Object if cls.name.toString=="base" => cls }
+    initialize(clss)
+    //var cf = featureMerge1(clss(0),clss(1))
+    println(classList)
 
 
   }
 
   def testMeta(): Unit = {
     val f =  source"""  import Collaboration._
-  object featurea {
+  object base {
    abstract class Graph {
     var a1 : Int = 0
     var a2 : Int = 1
